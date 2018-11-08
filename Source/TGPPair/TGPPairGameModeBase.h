84afbfad740ad06d434a12147ef5d7a3e222a91c
Daniel Bailey <41433945+LorenzoBee@users.noreply.github.com>
2018-11-08 11:25:39 +0000

    Initial commit

---
+++ Source/TGPPair/TGPPairGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TGPPairGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TGPPAIR_API ATGPPairGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
