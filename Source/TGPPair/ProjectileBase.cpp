8995fde830a45e517d1be80ff64a6eb5e69005b0
luke-burton <luke.burtos@outlook.com>
2019-01-06 17:14:50 +0000

    Added enemy with AI
    Enemy moves towards player, Shooting now interacts with enemy and enemy blows up on low hp.

--- Source/TGPPair/ProjectileBase.cpp
+++ Source/TGPPair/ProjectileBase.cpp
@@ -3,7 +3,15 @@
 #include "ProjectileBase.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/SphereComponent.h"
-
+#include "Enemy.h"
+#include "Engine.h"
+#include "Components/InputComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdexcept>
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 AProjectileBase::AProjectileBase()
@@ -14,7 +22,7 @@ AProjectileBase::AProjectileBase()
 	/*CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
 	CollisionSphere->SetSphereRadius(60.0f, true);
 	RootComponent = CollisionSphere;*/
-
+	isMoving = true;
 	Movespeed = 20.0f;
 	bulletDeathTimer = 0.0f;
 	bulletDeathMaxTime = 2.0f;
@@ -40,9 +48,42 @@ void AProjectileBase::Tick(float DeltaTime)
 		AddActorLocalOffset(FVector(Movespeed, 0.0f, 0.0f));
 		bulletDeathTimer += DeltaTime;
 
+
+		TArray<FHitResult>OutHits;
+		FVector ActorLocation = GetActorLocation();
+		FCollisionShape MyColSphere = FCollisionShape::MakeSphere(50.f);
+
+		bool isHit = GetWorld()->SweepMultiByChannel(OutHits, ActorLocation, ActorLocation, FQuat::Identity, ECC_WorldStatic, MyColSphere);
+		if (isHit)
+		{
+			for (auto& Hit : OutHits)
+			{
+				if (GEngine)
+				{
+					//	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, ("actor hit. %s" ,Hit.Actor->GetName()));
+					//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Hit result: %s", *Hit.Actor->GetName())));
+				}
+				if (Hit.Actor == NULL || Hit.Actor->GetName() == "")
+					continue;
+				if (Hit.Actor->GetName().Contains("enemy", ESearchCase::IgnoreCase, ESearchDir::FromStart))
+				{
+					if (Hit.Actor->GetName().Equals(this->GetName(), ESearchCase::IgnoreCase))
+						continue;
+					
+					Cast<AEnemy>(Hit.Actor)->Hurt(damage);
+					
+					Destroy();
+				}
+			}
+
+		}
+
+
 		if (bulletDeathTimer >= bulletDeathMaxTime && willDestroyItself)
 		{
+			isMoving = false;
 			Destroy();
 		}
 	}
+	
 }
